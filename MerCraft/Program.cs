56aca0f5585996f9f8d8ae67ae4e16d6178167f9
Merbo <carrethan02@gmail.com>
2012-06-15 13:10:42 -0400

    Merged some of icedream's stuff. Staying with .NET 4.5. Launcher now properly detects debug mode, and follows it. Deubgging will eventually be written better, as well as using WinAPI to embed the minecraft window to the form.

--- MerCraft/Program.cs
+++ MerCraft/Program.cs
@@ -11,12 +11,15 @@ namespace MerCraft
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        public static MainForm M;
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            M = new MainForm();
+            Application.Run(M);
         }
     }
 }
