8d7401093b7967300040e124d0a6126fa21da73d
Merbo <carrethan02@gmail.com>
2013-06-22 17:01:41 -0400

    Fix dependancies

--- MerCraft/Properties/AssemblyInfo.cs
+++ MerCraft/Properties/AssemblyInfo.cs
@@ -18,7 +18,8 @@ using System.Resources;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+//[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("da7afd60-274f-41a6-9947-d4b73bd204aa")]
